ff66030d97aed9355a2485f9c1f3dfcf56c67528
Ty Salter <62987363+saltty9188@users.noreply.github.com>
2021-05-31 21:57:06 +0930

    Clean Up Third Boss Code
    Added XML comments and refactored the scripts for the third boss.

--- Assets/Scripts/Bosses/Boss3/CloseEntrance.cs
+++ Assets/Scripts/Bosses/Boss3/CloseEntrance.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The CloseEntrance class is a trigger that closes a given RecordingDoor.
+/// </summary>
 public class CloseEntrance : MonoBehaviour
 {
     #region Inspector fields
-    [SerializeField] private RecordingDoor entrance;
+    [Tooltip("The Door to close.")]
+    [SerializeField] private RecordingDoor _entrance;
     #endregion
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player" || other.tag == "Clone")
         {
-            entrance.SetInFight(true);
+            _entrance.SetInFight(true);
         }
     }
 }
--- Assets/Scripts/Bosses/Boss3/StartThirdBoss.cs
+++ Assets/Scripts/Bosses/Boss3/StartThirdBoss.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The StartThirdBoss class is a trigger that causes the boss fight to begin.
+/// </summary>
 public class StartThirdBoss : MonoBehaviour
 {
     #region Inspector fields
-    [SerializeField] private ThirdBossScript bossScript;
-    [SerializeField] private GameObject bossUI;
-    [SerializeField] private RecordingDoor entrance;
+    [Tooltip("The main script for the third boss fight.")]
+    [SerializeField] private ThirdBossScript _bossScript;
+    [Tooltip("The GameObject that holds the boss UI.")]
+    [SerializeField] private GameObject _bossUI;
+    [Tooltip("The main entrance to the boss arena.")]
+    [SerializeField] private RecordingDoor _entrance;
     #endregion
 
     void OnTriggerEnter2D(Collider2D other) 
     {
+        // Start the fight when triggered
         if(other.tag == "Clone")
         {
-            bossScript.StartFight();
-            bossUI.SetActive(true);
+            _bossScript.StartFight();
+            _bossUI.SetActive(true);
         }
+        // Only close the entrance and start the boss fight if the player passes through the trigger
         else if(other.tag == "Player")
         {
-            bossScript.StartFight();
-            bossUI.SetActive(true);
-            entrance.SetInFight(true);
+            _bossScript.StartFight();
+            _bossUI.SetActive(true);
+            _entrance.SetInFight(true);
 
             AudioManager.Instance.PlayMusic("FinalBossTheme");
         }
